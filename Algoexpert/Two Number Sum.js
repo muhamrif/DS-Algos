eb4ab49e7a6fa9ddd39188bf90df66517a128daa
Muhammad Hamza <muhammad.hamza6415@gmail.com>
2022-12-19 09:23:56 -0500

    2sum done

---
+++ Algoexpert/Two Number Sum.js
@@ -0,0 +1,36 @@
+//Time Comlexity - O(n^2), Space Complexity - O(1)
+
+function twoNumberSum(array, target){
+
+    for (let i=0;i<array.length;i++){
+        for (let j=i+1;j<array.length;j++){
+            if (array[i]+array[j]===target){
+                return [array[i],array[j]]
+            }
+        }
+    }
+    return []
+}
+
+//___________________//
+
+//Time Comlexity - O(n), Space Complexity - O(n)
+
+function twoNumberSum(array, target){
+
+   let nums = {}
+
+   for(let i=0;i<array.length;i++){
+    let match = target - array[i]
+    if (nums[match]>=0){
+        return [match, array[i]]
+    }else{
+        nums[array[i]]= true
+    }
+   }
+
+    return []
+}
+
+//___________________//
+
