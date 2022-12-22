ff9f581bdc51e64b01d66e29f75b9a089b30eb9c
Muhammad Hamza <muhammad.hamza6415@gmail.com>
2022-12-22 07:45:26 -0500

    DFS added

--- Structy/Binary Tree/DepthFirstValues.js
+++ Structy/Binary Tree/DepthFirstValues.js
@@ -1,3 +1,28 @@
 //iterative strat is using a stack to keep a track of 'visited' nodes, and listing the children along with its own value.
 
 
+//iterative n runtime, n space
+const depthFirstValues2 = (root) => {
+    let result = []
+    let stack = [root]
+    while (stack.length>0){
+      const current = stack.pop()
+     if (current!== null) {
+      result.push(current.val)
+      if (current.right) stack.push(current.right)
+      if (current.left) stack.push(current.left)
+     }
+    }
+    return result
+  };
+
+  //recursive n reuntime, n space
+
+  const depthFirstValues = (root) => {
+    if (root===null) return []
+    const leftNodes = depthFirstValues(root.left)
+    const rightNodes = depthFirstValues(root.right)
+    return [root.val, ...leftNodes, ...rightNodes]
+  };
+
+  
