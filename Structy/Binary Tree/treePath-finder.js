eaad7e248640995a00a4562d8244ad06f6db8e2e
Muhammad Hamza <muhammad.hamza6415@gmail.com>
2022-12-26 09:02:46 -0500

    pathfinder

--- Structy/Binary Tree/treePath-finder.js
+++ Structy/Binary Tree/treePath-finder.js
@@ -1,1 +1,18 @@
 //give a root and a target, and have to find a path 
+
+const pathFinder = (root, target) => {
+    if (root ===null) return null
+    if (root.val ===target) return [root]
+
+    const leftPath = pathFinder(root.left, target)
+    if (leftPath){
+        return [root.val, ...leftPath]
+    }
+    const rightPath = pathFinder(root.right, target)
+    if (rightPath){
+        return [root.val, ...rightPath]
+    }
+
+    return null
+    
+}
