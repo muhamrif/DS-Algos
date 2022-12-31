f8b048812a6ef04474a24097c23fb41234df5ba4
Muhammad Hamza <muhammad.hamza6415@gmail.com>
2022-12-31 12:52:29 -0500

    tree tall

--- Structy/Binary Tree/howtall.js
+++ Structy/Binary Tree/howtall.js
@@ -1,1 +1,12 @@
 //check how tall is the tree, only given is the root node, we have to see how many nodeswe have to go through to get to the leaf node
+
+
+
+//recursive, n runtime, space
+const howHigh = (node) => {
+    if (node === null) return -1;
+  
+    const leftHeight = howHigh(node.left);
+    const rightHeight = howHigh(node.right);
+    return 1 + Math.max(leftHeight, rightHeight);
+  };
