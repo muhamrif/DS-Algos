40563e5602da5f17782964edbca9d05201650af9
Muhammad Hamza <muhammad.hamza6415@gmail.com>
2023-01-04 14:42:22 -0500

    bottom right val

---
+++ Structy/Binary Tree/bottom-right-value.js
@@ -0,0 +1,13 @@
+//iterative BFS, n time, n^2 space, because of the .shift()
+
+const bottomRightValue = (root) => {
+    let que = [root]
+    let current = null
+    while (que.length>0){
+       current = que.shift()
+      
+      if(current.left) que.push(current.left)
+      if(current.right)que.push(current.right)
+    }
+    return current.val
+  };
