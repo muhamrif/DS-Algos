80afe91fcc234d943b0a9a9dea17e3d3c4e69fbf
Muhammad Hamza <muhammad.hamza6415@gmail.com>
2023-01-10 22:49:55 -0500

    bottom right val

--- Structy/Binary Tree/BreadthFirstValues.js
+++ Structy/Binary Tree/BreadthFirstValues.js
@@ -11,9 +11,11 @@ const beadthFirstValues = (root)=>{
     const queue = [root]
 
     while (queue.length>0){
-        const current = queue.shift()
-        result.push(current.val)
         
+        const current = queue.shift()
+
+        result.push(current.val)
+
         if (current.left!== null) queue.push(current.left)
 
         if (current.right!==null) queue.push(current.right)
