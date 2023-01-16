e05d84b5a25ca3ff4bac59e7a8d269cb0a9cb551
Muhammad Hamza <muhammad.hamza6415@gmail.com>
2023-01-16 16:30:37 -0500

    2sum

--- Neetcode(python)/2sum.py
+++ Neetcode(python)/2sum.py
@@ -1,1 +1,17 @@
 print ('lets learn python')
+
+
+arr1 = [2,7,11,15]
+def func(arr, target):
+    for i in arr:
+        j = i+1
+        for j in arr:
+            if (i+j==target):
+                return [i,j]
+
+
+print(func(arr1, 9))
+   
+
+# func()
+
