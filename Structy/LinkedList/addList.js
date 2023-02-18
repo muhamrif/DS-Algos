b069a73cbfca2cccf3557a5fc3175954f445e646
Muhammad Hamza <muhammad.hamza6415@gmail.com>
2023-02-18 14:55:47 -0500

    comments

--- Structy/LinkedList/addList.js
+++ Structy/LinkedList/addList.js
@@ -36,17 +36,22 @@ const addLists1 = (head1, head2) => {
 
     //iterative optimised  max(n,m) time , max(n,m) space:  (whichever is larger, either head1 .lenght, or head2.lenght)
     const addLists2 = (head1, head2) => {
+
         const dummy = new Node(null)
+
         let tail = dummy
+
         let carry = 0 
         let current1 = head1
         let current2 = head2
         
         while (current1 !== null || current2 !== null || carry ===1){
+
           const val1 = current1 ===null ? 0 : current1.val
           const val2 = current2 ===null ? 0 : current2.val
           
           const sum = val1 + val2 + carry
+
           carry = sum >9 ? 1: 0
           const digit = sum %10
           
@@ -68,6 +73,7 @@ const addLists1 = (head1, head2) => {
   
 
     const addLists = (head1, head2, carry = 0) => {
+      
         if (head1===null && head2===null && carry===0) return null
         
         const val1 = head1 ===null ? 0 : head1.val
