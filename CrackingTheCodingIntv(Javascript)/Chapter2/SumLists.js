3f86fd318f4d203db81f46d28b975f49def5c601
Muhammad Hamza <muhammad.hamza6415@gmail.com>
2023-09-11 09:35:14 -0400

    algos

--- CrackingTheCodingIntv(Javascript)/Chapter2/SumLists.js
+++ CrackingTheCodingIntv(Javascript)/Chapter2/SumLists.js
@@ -85,3 +85,54 @@ function sumLinkedLists(node1, node2, carry=0){
 
 //regual
 
+var sumList = function(head1, head2) {
+  
+    var node1 = head1;
+    var node2 = head2;
+    var node3 = null;
+    var head3 = null;
+    
+    var ones;
+    var tens = 0;
+    var sum;
+    
+    while (node1 !== null && node2 !== null) {
+      if (node1 === null) {
+        sum = node2.value;
+      } else if (node2 === null) {
+        sum = node1.value;
+      } else {
+        sum = node1.value + node2.value;
+      }
+  
+      sum += tens;
+      ones = sum % 10;
+      if (node3 === null) {
+        head3 = new LinkedList(ones);
+        node3 = head3;
+      } else {
+        node3.next = new LinkedList(ones);
+        node3 = node3.next;
+      }
+  
+      tens = Math.floor(sum / 10);
+  
+      if (node1 !== null) {
+        node1 = node1.next;
+      }
+  
+      if (node2 !== null) {
+        node2 = node2.next;
+      }
+  
+    }
+    if (tens > 0) {
+      node3.next = new LinkedList(tens);
+      node3 = node3.next;
+    }
+  
+    return head3;
+  };
+  
+  /* TEST */
+  
