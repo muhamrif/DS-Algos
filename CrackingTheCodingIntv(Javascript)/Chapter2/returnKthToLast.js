6a3c961d46eeb9f205072e0f130107dc71c86e53
Muhammad Hamza <muhammad.hamza6415@gmail.com>
2023-09-14 09:31:23 -0400

    algos

---
+++ CrackingTheCodingIntv(Javascript)/Chapter2/returnKthToLast.js
@@ -0,0 +1,22 @@
+const LinkedList = require("../util/LinkedListX");
+
+var findKthToLast = function(k, list) {
+  // do iteratively
+  //define two pointers , fast and slow pointer
+  let fast = list.head
+  let slow = list.head
+
+  //Move fast pointer k steps in the linkedlist while slow remains at head
+  for(let i=0;i<k;i++){
+    if(fast === null) return null //k is larger than length of linked list
+    fast = fast.next
+  }
+  // move both pointers at the same time, slow pointer will exit at kth node from the end
+  while(fast !== null){
+    fast =fast.next
+    slow=slow.next
+
+  }
+  return slow
+
+};
