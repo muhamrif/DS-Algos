22606f5485238060dde2a1682217a778a10bfb99
Muhammad Hamza <muhammad.hamza6415@gmail.com>
2023-09-16 09:26:42 -0400

    algos

---
+++ CrackingTheCodingIntv(Javascript)/RemoveDuplicates.js
@@ -0,0 +1,28 @@
+// remiveDups redone
+const LinkedList = require("../util/LinkedListX");
+
+function removeDuplicates(list) {
+  const _set = new Set();
+  let cur = list.head;
+  let prev = null;
+  while (cur) {
+    if (_set.has(cur.value)) {
+      // duplicate found
+      // de-link it from the list
+      // cur jumps next but previous stays
+      // right behind cur (as always)
+      let elem = cur;
+      prev.next = cur.next;
+      cur = cur.next;
+      elem.next = null;
+    }
+    else {
+      // add to the set
+      _set.add(cur.value);
+      prev = cur;
+      cur = cur.next;
+    }
+  }
+
+  return list;
+}
