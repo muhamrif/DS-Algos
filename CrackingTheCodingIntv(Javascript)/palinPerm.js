dd6f1f9fd10f46b44b691c12cc051f3586023b1a
Muhammad Hamza <muhammad.hamza6415@gmail.com>
2023-09-22 09:03:35 -0400

    algos

--- CrackingTheCodingIntv(Javascript)/palinPerm.js
+++ CrackingTheCodingIntv(Javascript)/palinPerm.js
@@ -31,4 +31,20 @@ var palinPerm = function(string) {
 
   //Solution2
 
+  function palinPerm(s) {
+    const sanitized = s.toUpperCase().split(" ").join("");
+    const freq = new Map();
+    for (let i = 0; i < sanitized.length; i++) {
+      const char = sanitized.charAt(i);
+      const prevFreq = freq.get(char) || 0;
+      freq.set(char, prevFreq + 1);
+    }
+    let oddCount = 0;
+    for (let char of freq) {
+      if (char[1] % 2 !== 0) {
+        oddCount++;
+      }
+    }
+    return oddCount < 2;
+  }
   
